3fefbaf26a6be285719db3b7ae0b7c1c68bdca85
Ryan Shea <ryan@shea.io>
2016-03-10 20:29:21 -0500

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zone-file',
-    version='0.0.1',
+    version='0.1.0',
     url='https://github.com/blockstack/dns-zone-file-py',
     license='MIT',
     author='Blockstack Developers',
