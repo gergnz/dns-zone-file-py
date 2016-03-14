f4694fcc8f339b907098bb6875c33d8224f0dadc
Jude Nelson <judecn@gmail.com>
2016-03-14 18:03:03 -0400

    WIP: DNS zonefile parser

---
+++ zone_file/zone_file2.py
@@ -0,0 +1,662 @@
+#!/usr/bin/python 
+
+import copy
+import datetime
+import time
+import argparse
+from collections import defaultdict
+
+SUPPORTED_RECORDS = ['$ORIGIN', '$TTL', 'SOA', 'NS', 'A', 'AAAA', 'CNAME', 'MX', 'PTR', 'TXT', 'SRV', 'SPF', 'URI']
+
+"""
+Known limitations:
+    * only one $ORIGIN and one $TTL are supported
+    * only the IN class is supported
+    * PTR records must have a non-empty name
+    * currently only supports: '$ORIGIN', '$TTL', 'SOA', 'NS', 'A', 'AAAA', 'CNAME', 'MX', 'PTR', 'TXT', 'SRV', 'SPF', 'URI'
+"""
+
+defaultTemplate = """; Zone: {zone}\n\
+; Exported  (yyyy-mm-ddThh:mm:ss.sssZ): {datetime}\n\
+\n\
+{$origin}\n\
+{$ttl}\n\
+\n\
+; SOA Record\n\
+{name} {ttl}	IN	SOA	{mname} {rname} (\n\
+{serial} ;serial\n\
+{refresh} ;refresh\n\
+{retry} ;retry\n\
+{expire} ;expire\n\
+{minimum} ;minimum ttl\n\
+)\n\
+\n\
+; NS Records\n\
+{ns}\n\
+\n\
+; MX Records\n\
+{mx}\n\
+\n\
+; A Records\n\
+{a}\n\
+\n\
+; AAAA Records\n\
+{aaaa}\n\
+\n\
+; CNAME Records\n\
+{cname}\n\
+\n\
+; PTR Records\n\
+{ptr}\n\
+\n\
+; TXT Records\n\
+{txt}\n\
+\n\
+; SRV Records\n\
+{srv}\n\
+\n\
+; SPF Records\n\
+{spf}\n\
+\n\
+; URI Records\n\
+{uri}\n\
+"""
+
+
+def generate( options, template=None ):
+    """
+    Generate the DNS zonefile, given a dict-encoded
+    description of the zonefile (@options) and the
+    template to fill in (@template)
+    """
+
+    global defaultTemplate
+    if template is None:
+        template = defaultTemplate[:]
+
+    template = process_ORIGIN( options.get('$origin', None), template )
+    template = process_TTL( options.get('$ttl', None), template )
+    template = processSOA( options.get('soa', None), template )
+    template = processNS( options.get('ns', None), template )
+    template = processA( options.get('a', None), template )
+    template = processAAAA( options.get('aaaa', None), template )
+    template = processCNAME( options.get('cname', None), template )
+    template = processMX( options.get('mx', None), template )
+    template = processPTR( options.get('ptr', None), template )
+    template = processTXT( options.get('txt', None), template )
+    template = processSRV( options.get('srv', None), template )
+    template = processSPF( options.get('spf', None), template )
+    template = processURI( options.get('uri', None), template )
+    template = processValues( options, template )
+    return template
+
+
+def process_ORIGIN( data, template ):
+    """
+    Replace {$origin} in template with a serialized $ORIGIN record
+    """
+    ret = ""
+    if data is not None:
+        ret += "$ORIGIN %s" % data
+
+    return template.replace("{$origin}", ret)
+
+
+def process_TTL( data, template ):
+    """
+    Replace {$ttl} in template with a serialized $TTL record
+    """
+    ret = ""
+    if data is not None:
+        ret += "$TTL %s" % data
+
+    return template.replace("{$ttl}", ret)
+
+
+def processSOA( data, template ):
+    """
+    Replace {SOA} in template with a set of serialized SOA records
+    """
+    ret = template[:]
+    data_name = data.get('name', '@')
+    data_ttl = data.get('ttl', '')
+
+    for key in data.keys():
+        value = data[key]
+        ret = ret.replace('{' + str(key) + '}', str(value) + '\t')
+
+    return ret
+
+
+def quoteField( data, field ):
+    """
+    Quote a field in a list of DNS records.
+    Return the new data records.
+    """
+    if data is None:
+        return None 
+
+    data_dup = copy.deepcopy(data)
+    for i in xrange(0, len(data_dup)):
+        data_dup[field] = '"%s"' % data_dup[field]
+        data_dup[field] = data_dup[field].replace(";", "\;")
+
+    return data_dup
+
+
+def processRR( data, rectype, reckey, field, template ):
+    """
+    Meta method:
+    Replace $field in template with the serialized $rectype records,
+    using @reckey from each datum.
+    """
+    if data is None:
+        return template.replace(field, "")
+
+    if type(reckey) != list:
+        reckey = [reckey]
+
+    ret = ""
+    for i in xrange(0, len(data)):
+        ret += data[i].get('name', '@') + '\t'
+        if data[i].get('ttl') is not None:
+            ret += '%s\t' % data[i]['ttl']
+
+        ret += '\t%s\t%s\n' % (str(rectype), "\t".join([str(data[i][rk]) for rk in reckey]))
+    
+    return template.replace(field, ret)
+
+
+def processNS( data, template ):
+    """
+    Replace {ns} in template with the serialized NS records
+    """
+    return processRR( data, "NS", "host", "{ns}", template )
+
+
+def processA( data, template ):
+    """
+    Replace {a} in template with the serialized A records
+    """
+    return processRR( data, "A", "ip", "{a}", template)
+
+
+def processAAAA( data, template ):
+    """
+    Replace {aaaa} in template with the serialized A records
+    """
+    return processRR( data, "AAAA", "ip", "{aaaa}", template )
+
+
+def processCNAME( data, template ):
+    """
+    Replace {cname} in template with the serialized CNAME records
+    """
+    return processRR( data, "CNAME", "alias", "{cname}", template )
+
+
+def processMX( data, template ):
+    """
+    Replace {mx} in template with the serialized MX records
+    """
+    return processRR( data, "MX", ["preference", "host"], "{mx}", template)
+
+
+def processPTR( data, template ):
+    """
+    Replace {ptr} in template with the serialized PTR records
+    """
+    return processRR( data, "PTR", "host", "{ptr}", template)
+
+
+def processTXT( data, template ):
+    """
+    Replace {txt} in template with the serialized TXT records
+    """
+    # quote txt
+    data_dup = quoteField( data, "txt" )
+    return processRR( data_dup, "TXT", "txt", "{txt}", template)
+
+
+def processSRV( data, template ):
+    """
+    Replace {srv} in template with the serialized SRV records
+    """
+    return processRR( data, "SRV", ["priority", "weight", "port", "target"], "{srv}", template )
+
+
+def processSPF( data, template ):
+    """
+    Replace {spf} in template with the serialized SPF records
+    """
+    return processRR( data, "SPF", "data", "{spf}", template)
+
+
+def processURI( data, template ):
+    """
+    Replace {uri} in templtae with the serialized URI records
+    """
+    # quote target 
+    data_dup = quoteField( data, "target" )
+    return processRR( data_dup, "URI", ["priority", "weight", "target"], "{uri}", template )
+
+
+def processValues( options, template ):
+    """
+    Replace {zone} and {datetime} in zonefile template
+    """
+    template = template.replace("{zone}", options.get("$origin", options.get("soa", {}).get("name", "")) )
+    template = template.replace("{datetime}", datetime.date.fromtimestamp(time.time()).isoformat())
+    return template.replace("{time}", str(int(round( time.time() ))))
+
+
+def make_RR_subparser( subp, rec_type, args_and_types ):
+    """
+    Make a subparser for a given type of DNS record
+    """
+    sp = subp.add_parser(rec_type)
+
+    sp.add_argument("name", type=str)
+    sp.add_argument("ttl", type=int, nargs='?')
+    sp.add_argument(rec_type, type=str)
+
+    for (argname, argtype) in args_and_types:
+        sp.add_argument( argname, type=argtype )
+
+    return sp
+
+
+def make_parser():
+    """
+    Make an ArgumentParser that accepts DNS RRs
+    """
+    p = argparse.ArgumentParser()
+    subp = p.add_subparsers()
+
+    # parse $ORIGIN
+    sp = subp.add_parser("$ORIGIN")
+    sp.add_argument("$ORIGIN", type=str)
+
+    # parse $TTL
+    sp = subp.add_parser("$TTL")
+    sp.add_argument("$TTL", type=int)
+
+    # parse each RR
+    make_RR_subparser( subp, "SOA", [("mname", str), \
+                                     ("rname", str), \
+                                     ("serial", int), \
+                                     ("refresh", int), \
+                                     ("retry", int), \
+                                     ("expire", int), \
+                                     ("minimum", int)] )
+
+    make_RR_subparser( subp, "NS", [("host", str)] )
+    make_RR_subparser( subp, "A", [("ip", str)] )
+    make_RR_subparser( subp, "AAAA", [("ip", str)])
+    make_RR_subparser( subp, "CNAME", [("alias", str)])
+    make_RR_subparser( subp, "MX", [("preference", str), ("host", str)])
+    make_RR_subparser( subp, "TXT", [("txt", str)] )
+    make_RR_subparser( subp, "PTR", [("host", str)] )
+    make_RR_subparser( subp, "SRV", [("priority", int), ("weight", int), ("port", int), ("target", str)] )
+    make_RR_subparser( subp, "SPF", [("data", str)] )
+    make_RR_subparser( subp, "URI", [("priority", int), ("weight", int), ("target", str)] )
+
+    return p
+
+
+def tokenize( line ):
+    """
+    Tokenize a line:
+    * split tokens on whitespace
+    * treat quoted strings as a single token
+    * drop comments
+    * handle escaped spaces and comment delimiters
+    """
+    ret = []
+    escape = False
+    quote = False
+    tokbuf = ""
+    ll = list(line)
+    while len(ll) > 0:
+        c = ll.pop(0)
+        if c.isspace():
+            if not quote and not escape:
+                # end of token
+                if len(tokbuf) > 0:
+                    ret.append(tokbuf)
+
+                tokbuf = ""
+
+            elif quote:
+                # in quotes
+                tokbuf += c
+
+            elif escape:
+                # escaped space
+                tokbuf += c
+                escape = False
+
+            else:
+                tokbuf = ""
+
+            continue
+
+        if c == '\\':
+            escape = True
+            continue
+
+        elif c == '"':
+            if not escape:
+                if quote:
+                    # end of quote
+                    ret.append(tokbuf)
+                    tokbuf = ""
+                    quote = False
+                    continue
+
+                else:
+                    # beginning of quote
+                    quote = True
+                    continue
+
+        elif c == ';':
+            if not escape:
+                # comment 
+                ret.append(tokbuf)
+                tokbuf = ""
+                break
+            
+        # normal character
+        tokbuf += c
+        escape = False
+
+    if len(tokbuf.strip(" ").strip("\n")) > 0:
+        ret.append(tokbuf)
+
+    return ret
+
+
+def serialize( tokens ):
+    """
+    Serialize tokens:
+    * quote whitespace-containing tokens
+    * escape semicolons
+    """
+    ret = []
+    for tok in tokens:
+        if " " in tok:
+            tok = '"%s"' % tok
+
+        if ";" in tok:
+            tok = tok.replace(";", "\;")
+
+        ret.append(tok)
+
+    return " ".join(ret)
+
+
+def remove_comments( text ):
+    """
+    Remove comments from a zonefile
+    """
+    ret = []
+    lines = text.split("\n")
+    for line in lines:
+        if len(line) == 0:
+            continue 
+
+        line = serialize( tokenize( line ) )
+        ret.append(line)
+
+    return "\n".join(ret)
+
+
+def flatten( text ):
+    """
+    Flatten the text: make sure each record is on one line.
+    """
+    lines = text.split("\n")
+
+    # tokens: sequence of non-whitespace separated by '' where a newline was
+    tokens = []
+    for l in lines:
+        if len(l) == 0:
+            continue 
+
+        l = l.replace("\t", " ")
+        tokens += filter( lambda x: len(x) > 0, l.split(" ") ) + ['']
+
+    # find (...) and turn it into a single line ("capture" it)
+    capturing = False
+    captured = []
+
+    flattened = []
+    while len(tokens) > 0:
+        tok = tokens.pop(0)
+        if not capturing and len(tok) == 0:
+            # normal end-of-line
+            if len(captured) > 0:
+                flattened.append( " ".join(captured) )
+                captured = []
+            continue 
+
+        if tok.startswith("("):
+            # begin grouping
+            tok = tok.lstrip("(")
+            capturing = True
+
+        if capturing and tok.endswith(")"):
+            # end grouping.  next end-of-line will turn this sequence into a flat line
+            tok = tok.rstrip(")")
+            capturing = False 
+
+        captured.append(tok)
+
+    return "\n".join( flattened )
+
+
+def remove_class( text ):
+    """
+    Remove the CLASS from each DNS record, if present.
+    The only class that gets used today (for all intents
+    and purposes) is 'IN'.
+    """
+
+    # see RFC 1035 for list of classes
+    lines = text.split("\n")
+    ret = []
+    for line in lines:
+        tokens = tokenize( line )
+        if "IN" in tokens:
+            tokens.remove("IN")
+        elif "CS" in tokens:
+            tokens.remove("CS")
+        elif "CH" in tokens:
+            tokens.remove("CH")
+        elif "HS" in tokens:
+            tokens.remove("HS")
+
+        ret.append( serialize(tokens) )
+
+    return "\n".join(ret)
+
+
+def add_default_name( text ):
+    """
+    Go through each line of the text and ensure that 
+    a name is defined.  Use '@' if there is none.
+    """
+    global SUPPORTED_RECORDS
+
+    lines = text.split("\n")
+    ret = []
+    for line in lines:
+        tokens = tokenize( line )
+        if tokens[0] in SUPPORTED_RECORDS and not tokens[0].startswith("$"):
+            # add back the name
+            tokens = ['@'] + tokens 
+
+        ret.append( serialize(tokens) )
+
+    return "\n".join(ret)
+
+
+def parse_zone_file( text ):
+    """
+    Parse a zonefile into a dict
+    """
+    # print "======== before"
+    # print text
+    text = remove_comments( text )
+    # print "======== no comments"
+    # print text
+    text = flatten( text )
+    # print "======== flattened"
+    # print text
+    text = remove_class( text )
+    # print "======== no class"
+    # print text
+    text = add_default_name( text )
+    # print "======== default name"
+    # print text
+    return parse_lines( text )
+
+
+def parse_line( parser, RRtok, parsed_records ):
+    """
+    Given the parser, capitalized list of a line's tokens, and the current set of records 
+    parsed so far, parse it into a dictionary.
+
+    Return the new set of parsed records
+    """
+
+    global SUPPORTED_RECORDS
+
+    # match parser to record type
+    if len(RRtok) >= 2 and RRtok[1] in SUPPORTED_RECORDS:
+        # with no ttl
+        RRtok = [RRtok[1]] + RRtok
+
+    elif len(RRtok) >= 3 and RRtok[2] in SUPPORTED_RECORDS:
+        # with ttl
+        RRtok = [RRtok[2]] + RRtok
+
+    print RRtok
+
+    rr, unmatched = parser.parse_known_args( RRtok )
+    assert len(unmatched) == 0, "Unmatched fields: %s" % unmatched
+
+    rrd = rr.__dict__
+
+    # what kind of record? including origin and ttl
+    rectype = None
+    for key in rrd.keys():
+        if key in SUPPORTED_RECORDS and (key.startswith("$") or rrd[key] == key):
+            rectype = key
+            if rrd[key] == key:
+                del rrd[key]
+            break
+
+    assert rectype is not None, "Unknown record type in %s" % rr
+
+    # clean fields
+    for field in rrd.keys():
+        if rrd[field] is None:
+            del rrd[field]
+
+    current_origin = rrd.get('$ORIGIN', parsed_records.get('$ORIGIN', None))
+
+    # special record-specific fix-ups
+    """
+    if rectype == 'SPF':
+        rrd['data'] = " ".join(rrd['data'])
+
+    elif rectype == 'URI':
+        rrd['target'] = rrd['target'].strip('"')
+
+    elif rectype == 'TXT':
+        rrd['txt'] = rrd['txt'].strip('"')
+    """
+    if rectype == 'PTR':
+        rrd['fullname'] = rrd['name'] + '.' + current_origin
+      
+    if len(rrd) > 0:
+        if rectype.startswith("$"):
+            # put the value directly
+            parsed_records[rectype] = rrd[rectype]
+
+        else:
+            parsed_records[rectype].append( rrd )
+
+
+    return parsed_records
+
+
+def parse_lines( text ):
+    """
+    Parse a zonefile into a dict.
+    @text must be flattened--each record must be on one line.
+    Also, all comments must be removed.
+    """
+    ret = defaultdict(list)
+    rrs = text.split("\n")
+    parser = make_parser()
+
+    for rrtxt in rrs:
+        RR = rrtxt.upper()
+        RRtok = tokenize( RR )
+        ret = parse_line( parser, RRtok, ret )
+
+    return ret
+
+
+def make_zone_file( zonefile_data ):
+    """
+    Given zonefile data, export it to an actual zonefile.
+    zonefile_data = {
+        "$ORIGIN": origin server,
+        "$TTL": default time-to-live,
+        "SOA": [ soa records ],
+        "NS": [ ns records ],
+        "A": [ a records ],
+        "AAAA": [ aaaa records ]
+        "CNAME": [ cname records ]
+        "MX": [ mx records ]
+        "PTR": [ ptr records ]
+        "TXT": [ txt records ]
+        "SRV": [ srv records ]
+        "SPF": [ spf records ]
+        "URI": [ uri records ]
+    }
+    """
+    return generate( zonefile_data )
+
+
+if __name__ == "__main__":
+    import sys
+    import json
+    import traceback
+
+    if len(sys.argv) != 2:
+        print >> sys.stderr, "Usage: %s [txt or json file]" % sys.argv[0]
+        sys.exit(1)
+
+    dat = None
+    with open(sys.argv[1], "r") as f:
+        dat = f.read()
+
+    try:
+        # maybe it's a JSON file?
+        dat = json.loads(dat)
+        zf = make_zone_file( dat )
+        print zf
+
+    except ValueError:
+        # maybe it's a zone file?
+        zfj = parse_zone_file( dat )
+        print json.dumps(zfj, indent=4, sort_keys=True)
+
+    except Exception, e:
+        traceback.print_exc()
+        sys.exit(1)
+
