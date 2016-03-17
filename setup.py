5cc1e82b4a820eba34a938595433021c21d1c1b8
Ryan Shea <ryan@shea.io>
2016-03-17 18:42:34 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ DNS Zone File
 from setuptools import setup, find_packages
 
 setup(
-    name='zone-file',
-    version='0.1.3',
+    name='zonefile',
+    version='0.1.0',
     url='https://github.com/blockstack/dns-zone-file-py',
     license='MIT',
     author='Blockstack Developers',
