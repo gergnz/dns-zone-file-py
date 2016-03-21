5229fd78e41c606c25bcd39d553fbcaa8206daf9
Jude Nelson <judecn@gmail.com>
2016-03-21 15:22:12 -0400

    Restore origin= and ttl= keyword arguments; restore CAPITALIZED zonefile resource record names

--- zone_file/make_zone_file.py
+++ zone_file/make_zone_file.py
@@ -6,47 +6,53 @@ from .record_processors import (
 from .configs import DEFAULT_TEMPLATE
 
 
-def make_zone_file(json_zone_file, template=None):
+def make_zone_file(json_zone_file, origin=None, ttl=None, template=None):
     """
     Generate the DNS zonefile, given a json-encoded description of the
     zone file (@json_zone_file) and the template to fill in (@template)
 
     json_zone_file = {
-        "$origin": origin server,
-        "$ttl":    default time-to-live,
-        "soa":     [ soa records ],
-        "ns":      [ ns records ],
-        "a":       [ a records ],
-        "aaaa":    [ aaaa records ]
-        "cname":   [ cname records ]
-        "mx":      [ mx records ]
-        "ptr":     [ ptr records ]
-        "txt":     [ txt records ]
-        "srv":     [ srv records ]
-        "spf":     [ spf records ]
-        "uri":     [ uri records ]
+        "$ORIGIN": origin server,
+        "$TTL":    default time-to-live,
+        "SOA":     [ soa records ],
+        "NS":      [ ns records ],
+        "A":       [ a records ],
+        "AAAA":    [ aaaa records ]
+        "CNAME":   [ cname records ]
+        "MX":      [ mx records ]
+        "PTR":     [ ptr records ]
+        "TXT":     [ txt records ]
+        "SRV":     [ srv records ]
+        "SPF":     [ spf records ]
+        "URI":     [ uri records ]
     }
     """
 
     if template is None:
         template = DEFAULT_TEMPLATE[:]
 
-    soa_records = [json_zone_file.get('soa')] if json_zone_file.get('soa') else None
+    if origin is not None:
+        json_zone_file['$ORIGIN'] = origin
+
+    if ttl is not None:
+        json_zone_file['$TTL'] = ttl
+
+    soa_records = [json_zone_file.get('SOA')] if json_zone_file.get('SOA') else None
 
     zone_file = template
-    zone_file = process_origin(json_zone_file.get('$origin', None), zone_file)
-    zone_file = process_ttl(json_zone_file.get('$ttl', None), zone_file)
+    zone_file = process_origin(json_zone_file.get('$ORIGIN', None), zone_file)
+    zone_file = process_ttl(json_zone_file.get('$TTL', None), zone_file)
     zone_file = process_soa(soa_records, zone_file)
-    zone_file = process_ns(json_zone_file.get('ns', None), zone_file)
-    zone_file = process_a(json_zone_file.get('a', None), zone_file)
-    zone_file = process_aaaa(json_zone_file.get('aaaa', None), zone_file)
-    zone_file = process_cname(json_zone_file.get('cname', None), zone_file)
-    zone_file = process_mx(json_zone_file.get('mx', None), zone_file)
-    zone_file = process_ptr(json_zone_file.get('ptr', None), zone_file)
-    zone_file = process_txt(json_zone_file.get('txt', None), zone_file)
-    zone_file = process_srv(json_zone_file.get('srv', None), zone_file)
-    zone_file = process_spf(json_zone_file.get('spf', None), zone_file)
-    zone_file = process_uri(json_zone_file.get('uri', None), zone_file)
+    zone_file = process_ns(json_zone_file.get('NS', None), zone_file)
+    zone_file = process_a(json_zone_file.get('A', None), zone_file)
+    zone_file = process_aaaa(json_zone_file.get('AAAA', None), zone_file)
+    zone_file = process_cname(json_zone_file.get('CNAME', None), zone_file)
+    zone_file = process_mx(json_zone_file.get('MX', None), zone_file)
+    zone_file = process_ptr(json_zone_file.get('PTR', None), zone_file)
+    zone_file = process_txt(json_zone_file.get('TXT', None), zone_file)
+    zone_file = process_srv(json_zone_file.get('SRV', None), zone_file)
+    zone_file = process_spf(json_zone_file.get('SPF', None), zone_file)
+    zone_file = process_uri(json_zone_file.get('URI', None), zone_file)
 
     # remove newlines, but terminate with one
     zone_file = "\n".join(
