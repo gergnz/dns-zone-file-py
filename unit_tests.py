3578e00eec0f8370ca08adea21246c5bfccfff49
Donncha O'Cearbhaill <donncha@donncha.is>
2016-08-21 19:24:15 +0200

    Fix bug in test for URI record

--- unit_tests.py
+++ unit_tests.py
@@ -19,7 +19,7 @@ class ZoneFileTests(unittest.TestCase):
         self.assertTrue(isinstance(zone_file, (unicode, str)))
         self.assertTrue("$ORIGIN" in zone_file)
         self.assertTrue("$TTL" in zone_file)
-        self.assertTrue("@ 1D URI" in zone_file)
+        self.assertTrue("@ 1D IN URI" in zone_file)
 
     def test_zone_file_creation_2(self):
         json_zone_file = zone_file_objects["sample_2"]
